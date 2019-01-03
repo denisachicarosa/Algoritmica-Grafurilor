f4e5b1a9c245708c1384a2fda882ce4d40ebb6b2
Denisa <florinachicarosa22@gmail.com>
2019-01-03 19:57:41 +0200

    Add BF algorithm

---
+++ BF/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+#include<vector>
+#include <queue>
+using namespace std;
+
+vector<vector<int> > la;
+int n,m;
+vector<int> tata,viz,culoare;
+
+void citire()
+{
+    ifstream f("graf.in");
+    f>>n>>m;
+    viz.resize(n+1);
+    tata.resize(n+1);
+    culoare.resize(n+1);
+    la.resize(n+1);
+    for(int i=0;i<m;i++)
+    {
+        int x,y;
+        f>>x>>y;
+        la[x].push_back(y);
+        la[y].push_back(x);
+    }
+}
+
+void afisare()
+{
+   for(int i=1;i<=n;i++)
+    { for(int j=0;j<la[i].size();j++) cout<<la[i][j]<<" ";
+    cout<<endl;}
+}
+
+void initializare()
+{
+    for(int i=1;i<=n;i++)
+    {
+        culoare[i]=i;
+        tata[i]=0;
+        viz[i]=0;
+    }
+}
+
+
+void bfs(int s)
+{
+    queue<int> q;
+    q.push(s);
+    while(!q.empty())
+    {
+        int u=q.front();
+        q.pop();
+        cout<<u<<" ";
+        viz[u]=1;
+        for(int i=0;i<la[u].size();i++)
+        {
+        int v=la[u][i];
+        if(viz[v]==0)
+        {   viz[v]=1;
+            tata[v]=u;
+            q.push(v);
+        }
+
+        }
+    }
+}
+
+
+int main()
+{
+    citire();
+    initializare();
+    afisare();
+    cout<<endl;bfs(1);
+    cout<<endl;
+    for(int i=1;i<=n;i++) cout<<tata[i]<<" ";
+   //
+    return 0;
+}
